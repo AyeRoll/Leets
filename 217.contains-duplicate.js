d0c1cf7184e7c068b1f306b14e13d3ae089c57da
AyeRoll <adrian.t.rolling@wmich.edu>
2025-12-12 20:28:40 -0500

    70/77 very sad

--- 217.contains-duplicate.js
+++ 217.contains-duplicate.js
@@ -12,18 +12,18 @@
 var containsDuplicate = function(nums) {
     var i = 0;
     var twice = false;
-    const num_len = len(nums);
+    const num_len = nums.length;
 
-    while (twice == false && i < num_len){
+    while (i < num_len && twice == false){
         var y = i + 1;
         while(y < num_len && twice == false){
-            if(nums[i] = nums[y]) {
+            if(nums[i] == nums[y]) {
                 twice = true;
             } else {
                 y += 1;
             }
         }
-        i++;
+        i += 1;
     }
     return twice;
     
